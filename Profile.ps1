14b2cbce77fe94cd7bb115c98aa8d51918ef78de
Jonathan <jonathanelian64@gmail.com>
2026-03-17 00:33:13 -0700

    feat: actualizacion de funciones de utilidad en profile powershell

--- Profile.ps1
+++ Profile.ps1
@@ -1,5 +1,4 @@
-. $HOME/.config/aliases.ps1
-
+. $HOME/.config/aliases.ps1                                                                                          
 function Invoke-Starship-TransientFunction {
   &starship module character
 }
@@ -8,10 +7,24 @@ Invoke-Expression (&starship init powershell)
 
 Enable-TransientPrompt
 
-Import-Module -Name Terminal-Icons 
+Import-Module -Name Terminal-Icons
 
 Function cfgNeo {nvim $HOME\AppData\Local\nvim\}
 Function cfgNp {Set-Location $HOME\AppData\Local\nvim\}
+Function asm { cd 'C:/TASM' }
+Function goProg { cd 'E:/prog' }
+Function goNotes { cd 'E:\prog\manuscripts' }
+
+Function free {
+	$OS = Get-CimInstance Win32_OperatingSystem
+	$UsedMemoryGB = ([math]::Round(($OS.TotalVisibleMemorySize - $OS.FreePhysicalMemory) / 1024, 2))
+	$TotalMemoryGB = ([math]::Round($OS.TotalVisibleMemorySize / 1024, 2))
+	$ramUsagePercent = ([math]::Round((100 - ($OS.FreePhysicalMemory / $OS.TotalVisibleMemorySize) * 100), 2))
+
+	Write-Host "Total RAM: $TotalMemoryGB GB"
+	Write-Host "Used RAM: $UsedMemoryGB GB"
+	Write-Host "RAM Usage: $ramUsagePercent%"
+}
 
 Function open($obj) {
    if ( !$obj ){
@@ -20,35 +33,69 @@ Function open($obj) {
 
    if ( $obj -is [Int32]  ){
       Start-Process chrome "http://localhost:$obj"
-   } 
-   if ( $obj -is [String] ){
-      . $obj
    }
+   if ( $obj -is [String] ){
+      . "$pwd\$obj"
+   }
+}
 
+function VsOpen($path) {
+    if ($path -is [Int32]) {
+        echo  "Path invalid"
+        return
+    }
+
+    if ($path -eq ".") {
+        # current path
+        $name = (Get-ChildItem -Filter *.sln).name
+        Invoke-item $name
+    } else {
+        # other path
+        $name = (Get-ChildItem -Path $path -Filter *.sln).name
+        Invoke-item $name
+    }
 }
 
 Function poshConfig {nvim $HOME/.config/JonasTheme.json; Write-Host " "}
 
+Function cppDev { & 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Launch-VsDevShell.ps1' }
+
 Function which ($command) {
-	Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue; 
-	Write-Host " "
+        Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue;
+        Write-Host " "
 }
 
+
+Function devServer {
+    pnpm dev
+}
+
+Function orgFn {
+    python org.py
+}
+
+Set-Alias org orgFn
 Set-Alias -Name vcfg -value cfgNeo
 Set-Alias -Name pvc -value cfgNp
 Set-Alias -Name prog -value goProg
+Set-Alias -Name notes -value goNotes
+Set-Alias -Name ip -value ipconfig
 Set-Alias -Name ll ls
-Set-Alias -Name vim nvim
+Set-Alias -Name cpl cppDev
+Set-Alias -Name dev devServer
 Set-Alias pcfg poshConfig
 
 $env:bash='C:\Program Files\Git\usr\bin\bash.exe'
 
-Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function EndOfLine 
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function BeginningOfLine 
-Set-PSReadLineKeyHandler -Chord 'Ctrl+q' -Function DeleteLine
+Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function EndOfLine
+Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord 'Ctrl+u' -Function DeleteLine
 Set-PSReadLineKeyHandler -Chord 'Ctrl+h' -Function BackwardWord
 Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -Function ForwardWord
 
 Set-PSReadLineOption -Colors @{
     "String" = "#f39c12"
 }
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
